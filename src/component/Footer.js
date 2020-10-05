038c9699dd9d549a971e0747a35a40a9f739a461
mohsinali-Iot <mohsinali481997@gmail.com>
2020-10-05 12:37:44 +0500

    first change

---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 id="footer" style={{backgroundColor:'green'}}>Welcome From Footer</h1>
+            </div>
+        )
+    }
+}
+export default Footer;
